4eb3d81434fd4155be7304347897b9a665a72b35
gsksudheer <sudheer.gsk77@gmail.com>
2022-09-26 22:44:08 +0530

    first commit

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 export default async () => {
-    return mongoose.connect("mongodb+srv://sudheer:Pssword123@cluster0.wh9d1.mongodb.net/DB?retryWrites=true&w=majority", {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    });
+    return mongoose.connect(
+      "mongodb+srv://oralpath:Oralpath123@cluster0.msq551o.mongodb.net/oralpathDB?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 };
